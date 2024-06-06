6944abe8a73b8bf18267f973196dd43973a4a585
AldoRastrelli <aldanarastrelli@gmail.com>
2024-06-06 01:01:46 -0300

    Refactor Modules

--- NewsMobileApp/NewsMobileApp/Modules/TabBarController.swift
+++ NewsMobileApp/NewsMobileApp/Modules/TabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  TabBar.swift
+//  TabBarController.swift
 //  NewsMobileApp
 //
 //  Created by Aldana Rastrelli on 05/06/2024.
@@ -12,10 +12,10 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newsViewController = UIViewController() // TODO change this to news
+        let newsViewController = PostsViewController(viewModel: PostsViewModel()) // TODO change this to news
         newsViewController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper"), selectedImage: UIImage(systemName: "newspaper.fill"))
         
-        let friendsViewController = UIViewController() // TODO change this to friends
+        let friendsViewController = PostsViewController(viewModel: PostsViewModel()) // TODO change this to friends
         friendsViewController.tabBarItem = UITabBarItem(title: "Friends", image: UIImage(systemName: "person.2"), selectedImage: UIImage(systemName: "person.2.fill"))
         
         self.viewControllers = [newsViewController, friendsViewController]
