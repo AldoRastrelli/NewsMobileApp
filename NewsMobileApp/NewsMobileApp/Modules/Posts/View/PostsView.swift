dda0ec19d4d8ca02009c42691c59df20d0062448
AldoRastrelli <aldanarastrelli@gmail.com>
2024-06-06 02:44:18 -0300

    [CNX-7] Add Data in TableView

--- NewsMobileApp/NewsMobileApp/Modules/Posts/View/PostsView.swift
+++ NewsMobileApp/NewsMobileApp/Modules/Posts/View/PostsView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 final class PostsViewController: UIViewController {
     
@@ -13,6 +14,8 @@ final class PostsViewController: UIViewController {
     private var viewTitle = UILabel()
     private var searchBar = UISearchBar()
     private let tableView = UITableView()
+    
+    private var cancellables = Set<AnyCancellable>()
     
     init(viewModel: PostsViewModel) {
         self.viewModel = viewModel
@@ -23,13 +26,13 @@ final class PostsViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupTableView()
-//        setupBindings()
+        setupBindings()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     private func setupUI() {
         view.backgroundColor = .white
         // Add Title
@@ -39,7 +42,7 @@ final class PostsViewController: UIViewController {
         viewTitle.translatesAutoresizingMaskIntoConstraints = false
         let safeArea = view.safeAreaLayoutGuide
         viewTitle.anchor(top: safeArea.topAnchor, paddingTop: 10, leading: safeArea.leadingAnchor, paddingLeading: 24, trailing: safeArea.trailingAnchor, paddingTrailing: 24)
-
+        
         // Add SearchBar
         searchBar.placeholder = "Search"
         view.addSubview(searchBar)
@@ -52,15 +55,37 @@ final class PostsViewController: UIViewController {
         tableView.anchor(top: searchBar.bottomAnchor, paddingTop: 10, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor)
         // TODO: add cell
         // tableView.register(PostCell.self, forCellReuseIdentifier: PostCell.reuseIdentifier)
-//        tableView.dataSource = self
-//        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "PostCell")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
-//    private func setupBindings() {
-//        viewModel.posts.bind { [weak self] _ in
-//            DispatchQueue.main.async {
-//                self?.tableView.reloadData()
-//            }
-//        }
-//    }
+    private func setupBindings() {
+        viewModel.$posts.sink { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }.store(in: &cancellables)
+    }
+}
+
+
+extension PostsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+        let post = viewModel.posts[indexPath.row]
+        cell.textLabel?.text = post.title
+        return cell
+    }
+}
+
+extension PostsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = viewModel.posts[indexPath.row]
+        print(post)
+    }
 }
--- NewsMobileApp/NewsMobileApp/Modules/Posts/ViewModel/PostsViewModel.swift
+++ NewsMobileApp/NewsMobileApp/Modules/Posts/ViewModel/PostsViewModel.swift
@@ -10,7 +10,12 @@ import Foundation
 class PostsViewModel: ObservableObject {
     @Published var posts: [Post] = []
     
+    init() {
+        fetchPosts()
+    }
+    
     func fetchPosts() {
         // TODO: Fetch posts from API
+        posts = [Post(id: 1, slug: "hola", url: "url", title: "This is the title", content: "This is a content, and is longer", image: "imageurl", thumbnail: "thumbnail", status: "this is a status", category: "category here", publishedAt: "published at??", updatedAt: "updated date!!", userID: 1), Post(id: 2, slug: "hola", url: "url", title: "This is the title", content: "This is a content, and is longer", image: "imageurl", thumbnail: "thumbnail", status: "this is a status", category: "category here", publishedAt: "published at??", updatedAt: "updated date!!", userID: 2)]
     }
 }
