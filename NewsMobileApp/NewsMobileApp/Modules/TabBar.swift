c4f8ceebb75fd27183d39e721709cfc7c711963b
AldoRastrelli <aldanarastrelli@gmail.com>
2024-06-06 00:05:17 -0300

    [CNX-5] Add Tab bar

--- NewsMobileApp/NewsMobileApp/Modules/TabBar.swift
+++ NewsMobileApp/NewsMobileApp/Modules/TabBar.swift
@@ -5,4 +5,19 @@
 //  Created by Aldana Rastrelli on 05/06/2024.
 //
 
-import Foundation
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let newsViewController = UIViewController() // TODO change this to news
+        newsViewController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper"), selectedImage: UIImage(systemName: "newspaper.fill"))
+        
+        let friendsViewController = UIViewController() // TODO change this to friends
+        friendsViewController.tabBarItem = UITabBarItem(title: "Friends", image: UIImage(systemName: "person.2"), selectedImage: UIImage(systemName: "person.2.fill"))
+        
+        self.viewControllers = [newsViewController, friendsViewController]
+    }
+}
