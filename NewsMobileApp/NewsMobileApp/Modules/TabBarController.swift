e947e3208791d7a6322da658457741bf0995f129
AldoRastrelli <aldanarastrelli@gmail.com>
2024-06-06 14:09:55 -0300

    Refactor: TabBarController calls different modules

--- NewsMobileApp/NewsMobileApp/Modules/TabBarController.swift
+++ NewsMobileApp/NewsMobileApp/Modules/TabBarController.swift
@@ -12,10 +12,10 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newsViewController = PostsViewController(viewModel: PostsViewModel()) // TODO change this to news
+        let newsViewController = PostsModule.build()
         newsViewController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper"), selectedImage: UIImage(systemName: "newspaper.fill"))
         
-        let friendsViewController = PostsViewController(viewModel: PostsViewModel()) // TODO change this to friends
+        let friendsViewController = FriendsModule.build()
         friendsViewController.tabBarItem = UITabBarItem(title: "Friends", image: UIImage(systemName: "person.2"), selectedImage: UIImage(systemName: "person.2.fill"))
         
         self.viewControllers = [newsViewController, friendsViewController]
